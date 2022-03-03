76db10f31d1cc7c70da1d64301e7aef57dc125b2
danieltslee <danie.ts.lee@gmail.com>
2022-03-03 14:57:24 -0800

    Changed tree subnet list to a table read scene when launching ui

--- SpeedTreeAssetGenerator/speedTreeImportFbxController.py
+++ SpeedTreeAssetGenerator/speedTreeImportFbxController.py
@@ -20,23 +20,25 @@ class SpeedTreeFbxImporter(QtWidgets.QWidget):
                 self.layout.addWidget(self.ui)
                 self.setParent(hou.ui.mainQtWindow(), QtCore.Qt.Window)
 
+        # Set default directory path
         self.directoryPath = None
         hipDir = os.path.dirname(hou.hipFile.path())
         self.ui.directoryPath.setText(hipDir)
 
         self.ui.tableOfFoldersOnDisk.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
 
-        print(self.ui.matchSize.isChecked())
-        print(self.ui.convertToYup.isChecked())
+        # Detect tree subnets in scene
+        self.populateTreeSubnetTable()
 
         # Signal
         self.signals()
 
     def signals(self):
         self.ui.launchBrowser.pressed.connect(partial(self.treeDirectory))
-        self.ui.tableOfFoldersOnDisk.customContextMenuRequested.connect(self.tableMenu)
+        self.ui.tableOfFoldersOnDisk.customContextMenuRequested.connect(self.tableMenuTreesInDir)
 
     def treeDirectory(self):
+        """ Open file explorer to find the directory containing trees """
         # Browse to folder on dist
         fileExplore = QtWidgets.QFileDialog()
         self.directoryPath = fileExplore.getExistingDirectory(self,
@@ -47,13 +49,20 @@ class SpeedTreeFbxImporter(QtWidgets.QWidget):
         # Get formatted dictionary
         fbxImportFormat = fbxSubnet.getFbxFilesList(self.directoryPath)[0]
 
+        self.populateTreeDirTable(fbxImportFormat)
+
+    def populateTreeDirTable(self, fbxImportFormat):
+        """ Tree directory table from directoryPath """
         # List of tree subnet folder names
         fbxDirNames = list(fbxImportFormat.keys())
         # Set row count on table
         self.ui.tableOfFoldersOnDisk.setRowCount(len(fbxDirNames))
+
+        # If no folders containing fbxs were found
         if len(fbxDirNames) == 0:
             # TODO add button functionality
             okOrCancel = self.messageBox(self.directoryPath)
+
         # Populate table
         for importIndex in range(0, len(fbxDirNames)):
             # Populate rows
@@ -85,7 +94,8 @@ class SpeedTreeFbxImporter(QtWidgets.QWidget):
         else:
             return False
 
-    def tableMenu(self, pos):
+    def tableMenuTreesInDir(self, pos):
+        """ Right Click Menu for tableOfFoldersOnDisk """
         menu = QtWidgets.QMenu()
         self.fileAction = QtWidgets.QAction("Clear")
         self.fileAction.triggered.connect(self.reformatClearedTable)
@@ -93,24 +103,29 @@ class SpeedTreeFbxImporter(QtWidgets.QWidget):
         menu.exec_(self.ui.tableOfFoldersOnDisk.viewport().mapToGlobal(pos))
 
     def reformatClearedTable(self):
-        """numOfCol = self.ui.tableOfFoldersOnDisk.columnCount()
-        for colIndex in range(0, numOfCol):
-            self.ui.tableOfFoldersOnDisk."""
+        """ Clears all rows but keeps columns in a table widget """
         self.ui.tableOfFoldersOnDisk.clearContents()
+        # Clears rows
+        self.ui.tableOfFoldersOnDisk.setRowCount(0)
 
-        """# Clears rows  TODO HORIZONTAL HEADER STILL DOESN'T CLEAR
-        numOfRows = self.ui.tableOfFoldersOnDisk.rowCount()
-        for rowIndex in range(0, numOfRows):
-            self.ui.tableOfFoldersOnDisk"""
-
-"""    def populateTreeSubnetList(self):
+    def populateTreeSubnetTable(self):
         obj = hou.node("/obj")
-        self.ui.listOfTreeSubnets.clear()
-        
+        self.ui.tableOfTreeSubnets.clear()
+
         generatedNodes = []
-        for node in obj:
+        for node in obj.children():
             if node.creatorState() == "SpeedTree Asset Generator by Daniel":
-                
-        for selection in multipleSelections:
-            item = QtWidgets.QListWidgetItem(selection)
-            self.ui.listOfTreeSubnets.addItem(item)"""
+                generatedNodes.append(node)
+
+        if not generatedNodes:
+            self.ui.listOfTreeSubnets.addItem("No Tree Subnets Detected")
+            noItemsText = self.ui.listOfTreeSubnets.item(0)
+            noItemsText.setForeground(QtCore.Qt.red)
+            return
+
+        # Generate rows
+        self.ui.tableOfTreeSubnets.setRowCount(len(generatedNodes))
+        for nodeIndex in range(0, len(generatedNodes)):
+            generatedNodeNameObj = QtWidgets.QTableWidgetItem(generatedNodes[nodeIndex].name())
+            self.ui.tableOfTreeSubnets.setItem(nodeIndex, 0, generatedNodeNameObj)
+
